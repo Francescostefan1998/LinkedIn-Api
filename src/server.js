9b536ebff82cb73ac3775581d9427fa7975fbde9
Francisco-Fortes <fortesfco@gmail.com>
2023-01-25 10:20:44 +0100

    Upload profile pic added

--- src/server.js
+++ src/server.js
@@ -2,7 +2,14 @@ import express from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import mongoose from "mongoose";
-import experienceRouter from "./api/experiences";
+import experienceRouter from "./api/experiences/index.js";
+import usersRouter from "./api/users/index.js";
+import fileUserRouter from "./api/users/file/index.js";
+import {
+  badRequestErrorHandler,
+  notFoundErrorHandler,
+  genericErrorHandler,
+} from "./errorHandlers.js";
 
 const server = express();
 const port = process.env.PORT || 3001;
@@ -13,7 +20,8 @@ server.use(express.json());
 
 //Endpoints
 server.use("/users", usersRouter);
-server.use("users", experienceRouter);
+server.use("/users", experienceRouter);
+server.use("/users", fileUserRouter);
 
 //ErrorHandlers
 server.use(badRequestErrorHandler);
