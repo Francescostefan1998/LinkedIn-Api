e272bf941b7510621c021e083e9fb4708ed9dd7c
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-01-27 09:50:16 +0100

    fixing post user profile image

--- src/api/users/file/index.js
+++ src/api/users/file/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+/*import express from "express";
 import { v2 as cloudinary } from "cloudinary";
 import multer from "multer";
 import createHttpError from "http-errors";
@@ -40,7 +40,7 @@ fileUserRouter.post(
   }
 );
 
-export default fileUserRouter;
+export default fileUserRouter;*/
 
 // filesRouter.get("/pdf", async (req, res, next) => {
 //     res.setHeader("Content-Disposition", "attachment; filename=test.pdf")
@@ -52,3 +52,86 @@ export default fileUserRouter;
 //       if (err) console.log(err)
 //     })
 //   })
+
+import express from "express";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import UserModel from "../../users/model.js";
+import createHttpError from "http-errors";
+
+const fileUserRouter = express.Router();
+
+const cloudUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "users-s",
+    },
+  }),
+}).single("image");
+/*
+fileExperienceRouter.post(
+  "/profile/:userName/experiences/:expId/picture",
+  cloudUploader,
+  async (req, res, next) => {
+    try {
+      const user = await UserModel.findOne(
+        (user) => user.name === req.params.userName
+      );
+      if (user) {
+        console.log(user);
+        const experience = user.experiences.findOne(
+          (exp) => exp._id.toString() === req.params.expId
+        );
+        if (experience) {
+          const updatedProduct = await ExperienceModel.findByIdAndUpdate(
+            req.params.expId,
+            { image: req.file.path },
+
+            { new: true, runValidators: true }
+          );
+          res.send(updatedProduct);
+        } else {
+          next(
+            createHttpError(
+              404`Expirience with id ${req.params.expId} not found from user named ${req.params.userName}`
+            )
+          );
+        }
+      } else {
+        next(createHttpError(404`userName  ${req.params.userName} not found `));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default fileExperienceRouter;*/
+fileUserRouter.post(
+  "/:userId/picture",
+
+  cloudUploader,
+  async (req, res, next) => {
+    try {
+      const user = await UserModel.findById(req.params.userId);
+
+      if (user) {
+        console.log(user);
+        const updatedUser = await UserModel.findByIdAndUpdate(
+          req.params.userId,
+          { image: req.file.path },
+          { new: true, runValidators: true }
+        );
+        res.send(updatedUser);
+      } else {
+        next(createHttpError(404, `userName  ${req.params.userId} not found `));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default fileUserRouter;
